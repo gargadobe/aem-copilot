1c0026160d2813e4151c0c6bf1466066aad01bd8
Neeraj Garg <neeraj@neerajs-mbp.corp.adobe.com>
2024-06-12 12:20:34 +0530

    error handling

--- src/extension.ts
+++ src/extension.ts
@@ -30,18 +30,22 @@ export function activate(context: vscode.ExtensionContext) {
     token: vscode.CancellationToken
   ): Promise<IAemChatResult> => {
     let cmdResult: any;
-    if (request.command == commands.INFO) {
-      cmdResult = await infoCmdHandler(request, stream, token);
-    } else if (request.command == commands.CREATE) {
-      cmdResult = await createCmdHandler(request, stream, token);
-    } else if (request.command == commands.ENHANCE) {
-      cmdResult = await enhanceCmdHandler(request, stream, token);
-    } else if (request.command == commands.COLLECION) {
-      cmdResult = await fetchBlock(request, stream, token, context);
-    } else if (request.command == commands.DOCS) {
-      cmdResult = await handleDocsCommand(request, stream, token);
-    } else {
-      cmdResult = await defaultHandler(request, stream, token);
+    try {
+      if (request.command == commands.INFO) {
+        cmdResult = await infoCmdHandler(request, stream, token);
+      } else if (request.command == commands.CREATE) {
+        cmdResult = await createCmdHandler(request, stream, token);
+      } else if (request.command == commands.ENHANCE) {
+        cmdResult = await enhanceCmdHandler(request, stream, token);
+      } else if (request.command == commands.COLLECION) {
+        cmdResult = await fetchBlock(request, stream, token, context);
+      } else if (request.command == commands.DOCS) {
+        cmdResult = await handleDocsCommand(request, stream, token);
+      } else {
+        cmdResult = await defaultHandler(request, stream, token);
+      }
+    } catch (err) {
+      handleError(err, stream);
     }
 
     return cmdResult.metadata;
@@ -78,5 +82,21 @@ export function activate(context: vscode.ExtensionContext) {
     ),
   );
 }
+
+
+function handleError(err: any, stream: vscode.ChatResponseStream): void {
+  // making the chat request might fail because
+  // - model does not exist
+  // - user consent not given
+  // - quote limits exceeded
+  if (err instanceof vscode.LanguageModelError) {
+    console.log(err.message, err.code, err.cause);
+    if (err.cause instanceof Error && err.cause.message.includes('off_topic')) {
+      stream.markdown(vscode.l10n.t('I\'m sorry, I can only help with aem related topics.'));
+    }
+  }
+}
+
+
 
 export function deactivate() {}
